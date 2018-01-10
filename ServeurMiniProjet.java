15b4a25fedd9c96519e6eb9ceaa7d3cee8963e38
CVC <34715344+cvcarles@users.noreply.github.com>
2018-01-10 14:15:31 +0100

    10 javniver 14:15
    ecrit et shell ajouter 
    marche

--- ServeurMiniProjet.java
+++ ServeurMiniProjet.java
@@ -17,6 +17,37 @@ import java.io.File;
 		public int j=0;
 
 		
+		
+		
+		
+		private void writeline(File files) {
+		      os.println(file);
+		  }			
+	private void ecrit(String url) throws IOException{
+		FileInputStream f=new FileInputStream(""+url);
+		byte[] b=new byte[8];
+		while (f.read(b)>=0){write(b);}
+		f.close();
+		
+	}
+	private void shell(String url){
+		try{
+			Process myp = Runtime.getRuntime().exec(url);
+			myp.waitFor(); 	// attention, cette attente peut bloquer si la commande externe rencontre un problème
+			String line = null;
+			BufferedReader in = new BufferedReader(new InputStreamReader(myp.getInputStream()));
+			while((line = in.readLine()) != null) {
+			writeline(line);}
+			} 
+			catch(Exception e) {
+				System.out.println("error");
+
+	}
+	}
+	
+	
+	
+	
 		private void dialogue() throws IOException{
 			while (true){
 			acceptConn();
@@ -25,8 +56,12 @@ import java.io.File;
 			if (req!=null){
 				repRequete(req);
 			}
-			closeConn();}}
+			closeConn();
+			}
 			
+		}
+			
+		
 		public void repRequete(Requete req) throws IOException{
 
 			if (entier==1){																			// si l'URL est /date
@@ -47,23 +82,22 @@ import java.io.File;
 				for (int i = 0; i < liste.length; i++) {
 					if (Pattern .compile("^index.html$").matcher(liste[i]).find()){j=1;}    // si le répertoire pocède un fichier de type index.html ou .htm
 					else if (Pattern .compile("^index.htm$").matcher(liste[i]).find()){					
-						j=2;}}
-					
-					if (j==1){ecrit(this.file+"/index.html"); // ouvrir le fichier et l'afficher
-							}
-				else if (j==2){
-					/*FileInputStream f=new FileInputStream(this.file+"/index.htm");
-						byte[] b=new byte[8];
-						while (f.read(b)>=0){write(b);}
-						f.close();*/
-					ecrit(this.file+"/index.htm");}
+						j=2;
+					}
+				}
+					if (j==1){ecrit(this.file+"/index.html");} // ouvrir le fichier et l'afficher
+							
+				else if (j==2){ecrit(this.file+"/index.htm");}
 					
 				else {															// sinon lister le contenu du repertoire
 					writeline("<h1> Contenu du r&eacute;pertoire "+file+"</h1>");
 					for (int i = 0; i < liste.length; i++) {
-						writeline("<a href='"+file+"/"+liste[i]+"' >"+liste[i]+"</a><br>");}}}
+						writeline("<a href='"+file+"/"+liste[i]+"' >"+liste[i]+"</a><br>");
+						}
+					}
+			}
 			
-		
+			
 		else if (entier>2 && entier<=8){		// si l'URL pointe vers un fichier
 			writeline("HTTP/1.0 200 OK");	
 
@@ -75,35 +109,11 @@ import java.io.File;
 				else if (entier==8){writeline("content-type: application/pdf");}
 			
 			writeline("");
-				/*byte[] b=new byte[8];
-				FileInputStream f=new FileInputStream(this.file);
-				while (f.read(b)>=0){
-					write(b);}
-				f.close();*/
 			ecrit(""+this.file);
 					}
-			
-				
-			/*else if (entier==11){					// si l'URL pointe vers un shell
-				writeline("HTTP/1.0 200 OK");
-				writeline("content-type: text/html");
-				writeline("");
-				writeline("<!DOCTYPE html>");}*/}
-				
-			
-			
 		
-		private void writeline(File files) {
-			      os.println(file);
-			  }			
-		private void ecrit(String url) throws IOException{
-			FileInputStream f=new FileInputStream(""+url);
-			byte[] b=new byte[8];
-			while (f.read(b)>=0){write(b);}
-			f.close();
-			
 		}
-		
+				
 
 		public Requete getRequete() throws IOException{
 			String[] mots;
@@ -126,19 +136,11 @@ import java.io.File;
 				entier=1;
 			}
 			else if (Pattern .compile(".*/mon-cgi/.*.sh$").matcher(mots[1]).find()){ // en cas de shell passé en url
-				try{
+				
 					writeline("HTTP/1.0 200 OK");
-					Process myp = Runtime.getRuntime().exec(""+file);
-					myp.waitFor(); 	// attention, cette attente peut bloquer si la commande externe rencontre un problème
-					String line = null;
-					BufferedReader in = new BufferedReader(new InputStreamReader(myp.getInputStream()));
-					while((line = in.readLine()) != null) {
-					writeline(line);}
-					} 
-					catch(Exception e) {
-						System.out.println("error");
+					shell(""+file);
 
-			}
+			
 			}
 			
 			else if (Pattern .compile(".mon-cgi/.*.sh?").matcher(mots[1]).find()){ // en cas de shell avec des paramètres
@@ -147,46 +149,31 @@ import java.io.File;
 				String[] var = mots[1].split("\\?");		//mot[1] l'url entière
 				String[] parametre = var[1].split("\\&");	// va séparer les variables de leurs valeurs parametre[0] les variables et leurs valeurs associées avec le = au milieu
 				String resultat=("" +var[0]+" ");
-;
-		for (int i=0;i<parametre.length;i++ )	{
+				
+		for (int i=0;i<parametre.length;i++ )	{			// si des paramètres 
 
 				String[] val=parametre[i].split("=");
 				resultat += (" "+val[0]+" "+val[1]);
 
 		}
-				try{			// si des paramètres
-					Process myp = Runtime.getRuntime().exec(resultat); //lancer le shell avec des paramètres ex  var[0] le débute de l'url val[0]=mode et val[1]=cowthink
-					myp.waitFor(); 	// attention, cette attente peut bloquer si la commande externe rencontre un problème
-					String line = null;
-					BufferedReader in = new BufferedReader(new InputStreamReader(myp.getInputStream()));
-					while((line = in.readLine()) != null) {
-					writeline(line);}
-					
-					} 
-					catch(Exception e) {
-						writeline("error");
-			}
+				shell(resultat); //lancer le shell avec des paramètres ex  var[0] le débute de l'url val[0]=mode et val[1]=cowthink
+
+				}
 
 
 				
-		}
+		
 			else if (file.exists() && file.isDirectory()){		// vérifie que le répertoire existe
 				entier=2;
 				}	
 			else if (file.exists()){			// vérifie si ex: /home/user/Documents/salut existe
 				
-				if (Pattern .compile(".jpg$").matcher(mots[1]).find()){
-					entier=3;}
-				if (Pattern .compile(".png$").matcher(mots[1]).find()){
-					entier=4;}
-				if (Pattern .compile(".mp4$").matcher(mots[1]).find()){
-					entier=5;}
-				if (Pattern .compile(".html$").matcher(mots[1]).find()||Pattern .compile(".htm$").matcher(mots[1]).find()){
-					entier=6;}
-				if (Pattern .compile(".css$").matcher(mots[1]).find()){
-					entier=7;}
-				if (Pattern .compile(".pdf$").matcher(mots[1]).find()){
-					entier=8;}
+				if (Pattern .compile(".jpg$").matcher(mots[1]).find()){entier=3;}
+				if (Pattern .compile(".png$").matcher(mots[1]).find()){entier=4;}
+				if (Pattern .compile(".mp4$").matcher(mots[1]).find()){entier=5;}
+				if (Pattern .compile(".html$").matcher(mots[1]).find()||Pattern .compile(".htm$").matcher(mots[1]).find()){entier=6;}
+				if (Pattern .compile(".css$").matcher(mots[1]).find()){entier=7;}
+				if (Pattern .compile(".pdf$").matcher(mots[1]).find()){entier=8;}
 			}
 			
 			else{entier=0;}
