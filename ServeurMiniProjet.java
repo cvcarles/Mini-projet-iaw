56b433c1d50de068e68f5732c88ecc643ad67774
CVC <34715344+cvcarles@users.noreply.github.com>
2018-01-16 13:10:08 +0100

    16 janvier 13:10

--- ServeurMiniProjet.java
+++ ServeurMiniProjet.java
@@ -1,3 +1,4 @@
+// import des packages
 import java.io.*;
 import java.util.*;
 import java.util.regex.*;
@@ -6,33 +7,29 @@ import java.io.File;
 
 	public class ServeurMiniProjet extends Server{
 		public ServeurMiniProjet(int port,boolean verbose)throws IOException {
-			super(port,verbose);
+			super(port,verbose);				//appel du constructeur de la classe mère
 			String DataInputStream=null;
 			 
 			
 		}
-		public int entier;
+		public int entier;			// déclaration des variables 
 		public int i=0;
 		public int j=0;
 		public File file;
 
-
-		
-		
-		
 		
 					
-	private void ecrit(String url) throws IOException{
+	private void ecrit(String url) throws IOException{		// retranscrit le fichier sur le navigateur
 		FileInputStream f=new FileInputStream(""+url);
 		byte[] b=new byte[8];
 		while (f.read(b)>=0){write(b);}
 		f.close();
 		
 	}
-	private void shell(String url){
+	private void shell(String url){					// execute le shell passé en paramètre
 		try{
 			Process myp = Runtime.getRuntime().exec(url);
-			myp.waitFor(); 	// attention, cette attente peut bloquer si la commande externe rencontre un problème
+			myp.waitFor(); 						// attention, cette attente peut bloquer si la commande externe rencontre un problème
 			String line = null;
 			BufferedReader in = new BufferedReader(new InputStreamReader(myp.getInputStream()));
 			while((line = in.readLine()) != null) {
@@ -40,12 +37,8 @@ import java.io.File;
 			} 
 			catch(Exception e) {
 				System.out.println("error");
-
+			}
 	}
-	}
-	
-	
-	
 	
 		private void dialogue() throws IOException{
 			while (true){
@@ -54,13 +47,12 @@ import java.io.File;
 			req = getRequete();
 			if (req!=null){
 				repRequete(req);
-			}
+				}
 			closeConn();
 			}
 			
 		}
-			
-		
+				
 		public void repRequete(Requete req) throws IOException{
 			if (entier==1){																			// si l'URL est /date
 				SimpleDateFormat dateFormat = new SimpleDateFormat("EE dd MMM yyyy HH:mm:ss");
@@ -72,13 +64,13 @@ import java.io.File;
 				writeline(dateFormat.format(date));}
 			
 			else if (entier==2){													// si l'URL pointe vers un répertoire
-				writeline("HTTP/1.0 200 OK");							 
+				writeline("HTTP/1.0 200 OK");							 			
 				writeline("content-type: text/html");
 				writeline("");
 				writeline("<!DOCTYPE html>");
-				String liste[] = file.list();             				     
+				String liste[] = file.list();             				     		
 				for (int i = 0; i < liste.length; i++) {
-					if (Pattern .compile("^index.html$").matcher(liste[i]).find()){j=1;}    // si le répertoire pocède un fichier de type index.html ou .htm
+					if (Pattern .compile("^index.html$").matcher(liste[i]).find()){j=1;}    // si le répertoire possède un fichier de type index.html ou .htm on l'ouvre
 					else if (Pattern .compile("^index.htm$").matcher(liste[i]).find()){					
 						j=2;
 					}
@@ -87,7 +79,7 @@ import java.io.File;
 							
 				else if (j==2){ecrit(this.file+"/index.htm");}
 					
-				else {															// sinon lister le contenu du repertoire
+				else {															// sinon lister le contenu du répertoire
 					writeline("<h1> Contenu du r&eacute;pertoire "+file+"</h1>");
 					for (int i = 0; i < liste.length; i++) {
 						writeline("<a href='"+file+"/"+liste[i]+"' >"+liste[i]+"</a><br>");
@@ -109,29 +101,37 @@ import java.io.File;
 			writeline("");
 			ecrit(""+this.file);
 					}
-		
+		else if (entier==0){						//code d'erreur
+			writeline("HTTP/1.0 404 Not Found");
+			writeline("content-type: text/html");
+			writeline("");
+			writeline("<!DOCTYPE html>");
+			writeline("<html><body><h1>Votre fichier n'existe pas</h1>");
+			writeline("<img src=\"/home/user/Documents/mon-cgi/tantpis.png\" alt=\"tant pis\"></body></html>");
+							}
 		}
 				
 	
-		
 
 		public Requete getRequete() throws IOException{
 			String[] mots;
 			String ligne=readline();
 			if (ligne == null) return null;
 			if (ligne.isEmpty()) return null;
-			mots=ligne.split(" ");								//  créer un tableau en sérapant par en fonction des espaces
-			file = new File(mots[1]);		// file correspond au deuxième / de type File contenu dans le package java.io.File
+			mots=ligne.split(" ");								//  créer un tableau en sérapant la ligne en fonction des espaces
+			file = new File(mots[1]);		// file correspond au deuxième paramètre de type File contenu dans le package java.io.File
 			if (mots.length>1){
-				if (!mots[0].equals("GET") && !mots[0].equals("POST")){
+				if (!mots[0].equals("GET") && !mots[0].equals("POST")){		// si la requete n'est ni de type GET ni de type POST
 					return null;
 				}
-				while (ligne.isEmpty()==false){
+				while (ligne.isEmpty()==false){				// tant que la ligne n'est pas vide, la lire
 					ligne=readline();
 					if (ligne == null) return null;
 					}
 				
 		}
+// on assimile un entier à la variable "entier" selon les arguments de l'url
+			
 			if (mots[1].equals("/date")){
 				entier=1;
 			}
@@ -150,22 +150,20 @@ import java.io.File;
 				String[] parametre = var[1].split("\\&");	// va séparer les variables de leurs valeurs parametre[0] les variables et leurs valeurs associées avec le = au milieu
 				String resultat=("" +var[0]+" ");
 				
-		for (int i=0;i<parametre.length;i++ )	{			// si des paramètres 
+		for (int i=0;i<parametre.length;i++ )	{			// si le shell possède des paramètres
 
-				String[] val=parametre[i].split("=");
-				resultat += (" "+val[0]+" "+val[1]);
+				String[] val=parametre[i].split("=");		// on sépare chaque paramètre en fonction de "="
+				resultat += (" "+val[0]+" "+val[1]);		// on associe le paramètre à sa valeur
 
 		}
 				shell(resultat); //lancer le shell avec des paramètres ex  var[0] le débute de l'url val[0]=mode et val[1]=cowthink
 				entier=11;			// de manière à éviter qu'il ne le considère comme un répertoire
 				}
-
-
-				
-			else if (file.exists() && file.isDirectory()){		// vérifie que le répertoire existe
+		
+			else if (file.exists() && file.isDirectory()==true){		// vérifie que le répertoire existe
 				entier=2;
 				}	
-			else if (file.exists()){			// vérifie si ex: /home/user/Documents/salut existe
+			else if (file.exists()){			// vérifie si ex: /home/user/Documents/salut.jpg existe
 				
 				if (Pattern .compile(".jpg$").matcher(mots[1]).find()){entier=3;}
 				if (Pattern .compile(".png$").matcher(mots[1]).find()){entier=4;}
@@ -175,22 +173,21 @@ import java.io.File;
 				if (Pattern .compile(".pdf$").matcher(mots[1]).find()){entier=8;}
 			}
 			
-			else{entier=0;}
+			else{entier=0;}						// si le fichier ou le répertoire donné dans l'url n'existe pas, on renvoie l'entier renvoyant vers le message d'erreur
 			return new Requete(entier, mots[1]);
 		}
 		
 
 		
 		
-		
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException{		// fonction main du programme
 			ServeurMiniProjet myserver = null;
 			try {
 				myserver = new ServeurMiniProjet(1234, true);
 				} 
 			catch (IOException e) {
 				System.out.println("Problem while creating server!");
-				System.exit(-1); // code erreur <> 0 pour signaler qu'il y a 
+				System.exit(-1); // code erreur <> 0 pour signaler qu'il y a un problème 
 				}
 			try {
 				myserver.dialogue();
